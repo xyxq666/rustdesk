2cff1fc1308c95474af5306087818bd6bc50c9cc
xyxq666 <118185476+xyxq666@users.noreply.github.com>
2024-09-23 16:00:58 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -900,7 +900,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://aliyun.fl-re.cn".to_owned()
+    "http://aliyun.fl-re.cn:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
